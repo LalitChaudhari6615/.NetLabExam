7e51172136ad8adc9602408f3c2029e024f65ba8
Lalit R C <lalitrchaudhari6615@gmail.com>
2023-07-16 14:20:41 +0530

    first commit

---
+++ MenuDrivencode/Program.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MenuDrivenDatabaseProgram
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Update");
+                Console.WriteLine("2. Delete");
+                Console.WriteLine("3. View Data");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Update();
+                        break;
+                    case "2":
+                        Delete();
+                        break;
+                    case "3":
+                        DataReader();
+                        break;                    
+                    case "4":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+     
+        static void Update()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Module;
+            Integrated Security=True";
+            cn.Open();
+
+            try
+            {
+                Console.Write("Enter the Student rollno ID to update: ");
+                int Rollno = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the new Name: ");
+                string newName = Console.ReadLine();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "UPDATE students SET name = @NewName WHERE rollno = @Rollno";
+                cmd.Parameters.AddWithValue("@NewName", newName);
+                cmd.Parameters.AddWithValue("@Rollno", Rollno);
+
+
+                Console.WriteLine(cmd.ExecuteNonQuery());
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        static void Delete()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Module;
+              Integrated Security=True";
+            cn.Open();
+
+            try
+            {
+                Console.Write("Enter the student rollno to delete: ");
+                int Rollno = int.Parse(Console.ReadLine());
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "DELETE FROM students WHERE rollno = @Rollno";
+                cmd.Parameters.AddWithValue("@Rollno", Rollno);
+
+
+                Console.WriteLine(cmd.ExecuteNonQuery());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        static void DataReader()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Module;
+           Integrated Security=True";
+            cn.Open();
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from students";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+
+                    Console.Write(dr["rollno"] + " ");
+                    Console.Write(dr["name"] + " ");
+                    Console.Write(dr["marks"] + " ");
+                   
+                    Console.WriteLine();
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+
+    class Students
+    {
+        public string name { get; set; }
+        public int rollno { get; set; }
+        public int marks { get; set; }     
+    }
+}
---
+++ ModelBinding/Controllers/CarController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models.ModleBindingLab.Models;
+
+namespace ModelBinding.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: CarController
+        public ActionResult Index()
+        {
+            List<Car> lstcar = Car.GetAllCars();
+            return View(lstcar);
+        }
+
+
+        // GET: CarController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CarController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Car obj = Car.GetSingleCar(id);
+            return View(obj);
+        }
+
+        // POST: CarController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Car cr, IFormCollection collection)
+        {
+            try
+            {
+                Car.Update(cr);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Car obj = Car.GetSingleCar(id);
+            return View(obj);
+        }
+
+        // POST: CarController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Car obj)
+        {
+            try
+            {
+                Car.DeleteCar(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
